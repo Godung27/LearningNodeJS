763b3b5c1869e1c4956e65b1572b9ee97fe3bd5e
Godung27 <parnuvich.l@gmail.com>
2025-12-19 17:34:25 +0700

    Javascript Refresher_Refreshing the Core Syntax

---
+++ 02-Javascript-Refresher/play.js
@@ -0,0 +1,9 @@
+var name = "Godung";
+var age = 27;
+var isMan = true;
+
+function user(name, age, isMan) {
+  return (`User name ${name}, age ${age}, is a man: ${isMan}`);
+}
+
+console.log(user(name, age, isMan));
