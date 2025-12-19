5201b391f743af00ac962cda5bc713a8a258c593
Godung27 <parnuvich.l@gmail.com>
2025-12-19 17:42:17 +0700

    Javascript Refresher_Understanding Arrow Functions

--- 02-Javascript-Refresher/play.js
+++ 02-Javascript-Refresher/play.js
@@ -8,4 +8,9 @@ function user(name, age, isMan) {
   return (`User name ${name}, age ${age}, is a man: ${isMan}`);
 }
 
+const addArr = (a, b) => a + b;
+const addOneArr = a => a + 1;
+const num = () => 1 + 2;
+
 console.log(user(name, age, isMan));
+console.log(addArr(3, 4), addOneArr(1), num());
