68243eb10940b8e23c0c05f235bfaa573e939a33
Godung27 <parnuvich.l@gmail.com>
2025-12-20 16:46:31 +0700

    Javascript Refresher_Destructuring

--- 02-Javascript-Refresher/newPlay.js
+++ 02-Javascript-Refresher/newPlay.js
@@ -11,7 +11,18 @@ console.log(person.greet());
 const copyPerson = { ...person }
 console.log(copyPerson);
 
+const { name, age } = person;
+console.log(name, age);
+
+const personName = function ({ name }) {
+  console.log(name);
+}
+personName(person);
+
 const hobbies = ["Game", "Climbing"];
+const [hobby1, hobby2] = hobbies;
+
+console.log(hobby1);
 
 console.log(hobbies.map(hobby => "My Hobby: " + hobby));
 console.log(hobbies);
