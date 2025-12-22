4125479852fa7c15e5339fe3cc1822d408e2e789
Godung27 <parnuvich.l@gmail.com>
2025-12-22 17:23:55 +0700

    Nodejs Basics_Sending Responses

--- 03-Nodejs-Basics/app.js
+++ 03-Nodejs-Basics/app.js
@@ -3,6 +3,13 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
   // process.exit();
+
+  res.setHeader("Content-type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
 }, 3000);
 
 server.listen(3000);
