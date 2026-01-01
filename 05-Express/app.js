1d3bb4bb04bfa379e8797e2874e34b3bfd05bcce
Godung27 <parnuvich.l@gmail.com>
2026-01-01 18:06:51 +0700

    Express_Adding Middleware

--- 05-Express/app.js
+++ 05-Express/app.js
@@ -2,6 +2,17 @@ const http = require("http");
 
 const express = require("express");
 
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("middleware");
+  next(); // allow the request to continue to next middleware in line
+})
+
+app.use((req, res, next) => {
+  console.log("Next middleware");
+})
+
 const server = http.createServer(app);
 
 server.listen(3000);
