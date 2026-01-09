8ba53703efdcdad78c72256255541cf113467685
Godung27 <parnuvich.l@gmail.com>
2026-01-09 15:23:14 +0700

    Express_Parsing Incoming Requests

--- 05-Express/app.js
+++ 05-Express/app.js
@@ -1,18 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("always work");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send("<form action='/product' method='POST'><input type='text' name='title'/><button type='submit'>Add Product</button></form>")
 })
 
-app.use("/about", (req, res, next) => {
-  res.send("<h1>This is About page!</h1>")
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 })
 
 app.use("/", (req, res, next) => {
-  console.log("Next middleware");
   res.send("<h1>Hello from Express!</h1>")
 })
 
