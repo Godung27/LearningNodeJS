45aa739de872e17dae86df930ceefd7bd9d10cee
Godung27 <parnuvich.l@gmail.com>
2026-01-16 17:53:21 +0700

    Dynamic Content_Converting our Project to Handlebars

--- 06-Dynamic-Content/routes/shop.js
+++ 06-Dynamic-Content/routes/shop.js
@@ -7,9 +7,9 @@ const adminData = require("./admin.js");
 
 const router = express.Router();
 
-// router.get('/', (req, res, next) => {
-//   const products = adminData.products;
-//   res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
-// });
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render("shop", { prods: products, pageTitle: "Shop", path: "/", hasProducts: products.length > 0 });
+});
 
 module.exports = router;
