40efb69931f51432e698963b8a8e3cb48cfbc842
Godung27 <parnuvich.l@gmail.com>
2026-02-11 01:19:58 +0700

    Sequelize_Deleting Products

--- 11-Sequelize/controllers/admin.js
+++ 11-Sequelize/controllers/admin.js
@@ -20,7 +20,9 @@ exports.postAddProduct = (req, res, next) => {
     description: description,
   })
     .then(result => {
-      console.log(result);
+      // console.log(result);
+      console.log("CREATE PRODUCT");
+      res.redirect("/admin/products");
     })
     .catch(err => {
       console.log(err);
@@ -63,7 +65,7 @@ exports.postEditProduct = (req, res, next) => {
       return product.save();
     })
     .then(result => {
-      console.log(result);
+      console.log("UPDATED PRODUCT");
       res.redirect('/admin/products');
     })
     .catch(err => console.log(err));
@@ -83,6 +85,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  Product.findByPk(prodId)
+    .then(product => {
+      return product.destroy();
+    })
+    .then(result => {
+      console.log("DESTROYED PRODUCT");
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
 };
